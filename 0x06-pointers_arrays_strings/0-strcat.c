5b1dfe8b8356345e60ed82c79b56c97efa46eccf
Sam Hadey <ilesanmiaderibigbe@gmail.com>
2022-07-15 10:48:53 +0100

    mod 0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,9 +2,39 @@
 
 
 char *_strcat(char *dest, char *str)
-{	char fullStr[];
+{
+	char fullStr[];
 
+    int strLen = (strlen(dest) + strlen[str]);
+    int dest_len = strlen(dest);
 
-	
+	int i = 0;
+	while(dest[i] != '\0')
 	{
-		if(dest
+		fullStr[i] = dest[i];
+		i++;
+	}
+
+	for(int i = 0; i <= dest_len; i++){
+		if (dest[i] != '\0')
+		{
+			fullStr[i] = dest[i];
+			i++;
+		}else
+		{
+			fullStr[i] = " ";
+		}
+	}
+    
+
+	for(int i = 0; i <= str_len; i++){
+		if (dest[i] != '\0')
+		{
+			fullStr[i] = dest[i];
+			i++;
+		}else
+		{
+			putchar('\n') ;
+		}
+	}
+}
