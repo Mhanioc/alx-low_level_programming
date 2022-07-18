6b67c9c6a9bd19f45da4c413b9df09668f0becdd
Mhanioc <ilesanmiaderibigbe@gmail.com>
2022-07-18 08:57:00 -0700

    hello

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,20 +3,17 @@
 
 char *_strcat(char *dest, char *str)
 {
-
-int len = 0,i;
+int len = 0, i;
 
 while (dest[len])
 {
-i++;
+len++;
 
-for (int i = 0; str[i]; i++)
+for (i = 0; str[i]; i++)
 {
 dest[len] = str[i];
 }
-
 dest[len] = '\0';
 return dest;
 }
-	
 }
